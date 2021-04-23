dc902723db3e0e5c826c3e0f35586eb353623690
Felix Bröhl <35146235+felixbroehl@users.noreply.github.com>
2021-04-23 12:19:37 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-new-item -Name <foldername> -ItemType workdir
+new-item -Name workdir -ItemType directory
 wget "https://raw.githubusercontent.com/felixbroehl/bnfc-pli/main/Dockerfile" --outfile "Dockerfile"
 wget "https://raw.githubusercontent.com/felixbroehl/bnfc-pli/main/run.ps1" --outfile "run.ps1"
 docker build -t richard/bnfc . 
