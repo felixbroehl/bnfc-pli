8bdc41cb6f19a3ae5794a37fb4fe1d47aff94871
Felix Bröhl <35146235+felixbroehl@users.noreply.github.com>
2021-04-23 13:55:19 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 function brun {
-    docker run -it --rm -v "${PSScriptRoot}:/workdir" -w /workdir richard/bnfc:latest /bin/sh -c @args
+    docker run -it --rm -v "$(($pwd).path)\workdir:/workdir" -w /workdir richard/bnfc:latest /bin/sh -c @args
 }
