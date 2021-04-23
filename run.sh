b22613e2d1beb42f84199d556795fe55392d4f88
Felix Bröhl <35146235+felixbroehl@users.noreply.github.com>
2021-04-23 11:32:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-alias brun="docker run -it --rm -v ./workdir:/workdir -w /workdir --user $UID richard/bnfc:latest /bin/sh -c"
+alias brun="docker run -it --rm -v $PWD/workdir:/workdir -w /workdir --user $UID richard/bnfc:latest /bin/sh -c"
